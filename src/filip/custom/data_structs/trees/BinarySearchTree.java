590f9649ebec95c06d86673ee6f989999cd25a2a
filip <filip.kozjak@gmail.com>
2017-10-16 12:21:29 +0200

    Initial commit with initialized BST interface

---
+++ src/filip/custom/data_structs/trees/BinarySearchTree.java
@@ -0,0 +1,15 @@
+package filip.custom.data_structs.trees;
+
+/**
+ * Interface that represents any kind of a <b>Binary Search Tree</b>.<br>
+ * 
+ * This type of data structure is often also called <b>Ordered Binary Tree</b> 
+ * or <b>Sorted Binary Tree</b> as the elements in such data structures are stored in
+ * a sorted manner to allow faster lookup, addition or removal of elements.
+ * 
+ * @author fiilip
+ *
+ */
+public interface BinarySearchTree {
+
+}
